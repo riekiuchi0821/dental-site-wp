93b2f6c6ea1fc7248683ffdbd0fd6384663cc989
riekiuchi0821 <rie.kiuchi.studio@gmail.com>
2026-08-17 09:56:05 +0900

    パンくずリストのパーツをbreadcrumb.phpとして作成

---
+++ breadcrumb.php
@@ -0,0 +1,12 @@
+        <!-- パンくず-->
+        <div class="container section">
+          <nav class="breadcrumb" aria-label="パンくずリスト">
+            <ol class="breadcrumb-list">
+              <li><a href="<?php echo esc_url(home_url('/')); ?>">ホーム</a></li>
+              <li aria-current="page">
+                <?php the_title(); ?>
+              </li>
+            </ol>
+          </nav>
+        </div>
+        <!-- パンくずここまで-->
