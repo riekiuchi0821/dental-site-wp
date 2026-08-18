48f1492586d1151310028522f71207e677212771
riekiuchi0821 <rie.kiuchi.studio@gmail.com>
2026-08-18 10:11:23 +0900

    ブログ記事一覧のhome.phpを追加

---
+++ home.php
@@ -0,0 +1,66 @@
+<?php get_header(); ?>
+
+<main>
+    <!-- First View -->
+    <section id="fv" class="fv fv-subpage">
+        <picture class="fv-area">
+            <img class="fv-image fv-image--center" src="<?php echo get_template_directory_uri(); ?>/img/lobby.jpg"
+                alt="受付" />
+        </picture>
+        <div class="fv-copy fv-copy-subpage">
+            <h1 class="fv-message-subpage"><?php single_cat_title(); ?></h1>
+        </div>
+    </section>
+    <!-- First View ここまで-->
+
+    <div class="blog-page section-background-img">
+        <div class="container">
+            <div class="section">
+                <!-- パンくず-->
+                <?php get_template_part('breadcrumb'); ?>
+                <!-- パンくずここまで-->
+            </div>
+
+            <div class="blog-layout section">
+                <!-- ブログ -->
+                <section class="blog-list">
+
+                    <?php if (have_posts()): ?>
+                        <?php while (have_posts()):
+                            the_post(); ?>
+                            <article class="blog-card blog-card-background">
+                                <div class="blog-card-info">
+                                    <p><?php echo get_the_date('Y.m.d'); ?></p>
+                                    <p class="blog-card-tag"><?php echo esc_html(get_the_category()[0]->name); ?></p>
+                                </div>
+                                <h2 class="blog-card-title"><?php the_title(); ?></h2>
+                                <div class="card-divider"></div>
+                                <p>
+                                    <?php echo wp_trim_words(get_the_content(), 60, '...'); ?>
+                                </p>
+                                <div class="blog-button">
+                                    <a href="<?php the_permalink(); ?>" class="primary-button">詳細へ →</a>
+                                </div>
+                            </article>
+                        <?php endwhile; ?>
+                    <?php endif; ?>
+
+
+
+                    <div class="blog-pagination">
+                        <p>＜＜　1 ＞＞</p>
+                    </div>
+                </section>
+                <!-- ブログここまで-->
+
+                <!-- サイドバー -->
+                <?php get_sidebar(); ?>
+            </div>
+        </div>
+    </div>
+
+
+
+</main>
+
+<?php get_footer(); ?>
